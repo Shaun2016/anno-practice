06fd1dc6cfd50d0e62217fe52ec28ed08b5c4458
maoqiao <maoqiao@pinduoduo.com>
2023-04-01 23:33:38 +0800

    anno practice

---
+++ src/main/java/org/example/anno/SetterProcessor.java
@@ -0,0 +1,98 @@
+package org.example.anno;
+
+import com.sun.source.tree.Tree;
+import com.sun.tools.javac.api.JavacTrees;
+import com.sun.tools.javac.code.Flags;
+import com.sun.tools.javac.processing.JavacProcessingEnvironment;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.TreeMaker;
+import com.sun.tools.javac.tree.TreeTranslator;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.List;
+import com.sun.tools.javac.util.ListBuffer;
+import com.sun.tools.javac.util.Name;
+import com.sun.tools.javac.util.Names;
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+
+
+/**
+ * Setter注解处理器
+ */
+
+@SupportedAnnotationTypes(value = {"org.example.anno.Setter"})
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class SetterProcessor extends AbstractProcessor {
+
+    private Messager messager;
+
+    // 待处理的抽象语法树
+    private JavacTrees trees;
+
+    // 封装了操作AST的方法
+    private TreeMaker treeMaker;
+
+    // 提供了创建标识符的方法
+    private Names names;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        this.messager = processingEnv.getMessager();
+        this.trees = JavacTrees.instance(processingEnv);
+        Context context = ((JavacProcessingEnvironment)processingEnv).getContext();
+        this.treeMaker = TreeMaker.instance(context);
+        this.names = Names.instance(context);
+    }
+
+    /**
+     * 操作 AST
+     */
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Set<? extends Element> set = roundEnv.getElementsAnnotatedWith(Getter.class);
+        set.forEach(element -> {
+            JCTree jcTree = trees.getTree(element);
+            jcTree.accept(new TreeTranslator() {
+                @Override
+                public void visitClassDef(JCTree.JCClassDecl jcClassDecl) {
+                    List<JCTree.JCVariableDecl> jcVariableDeclList = List.nil();
+                    for (JCTree tree : jcClassDecl.defs) {
+                        if (tree.getKind().equals(Tree.Kind.VARIABLE)) {
+                            JCTree.JCVariableDecl jcVariableDecl = (JCTree.JCVariableDecl) tree;
+                            jcVariableDeclList = jcVariableDeclList.append(jcVariableDecl);
+                        }
+                    }
+                    jcVariableDeclList.forEach(jcVariableDecl -> {
+                        messager.printMessage(Diagnostic.Kind.NOTE, jcVariableDecl.getName() + " has been processed");
+                        jcClassDecl.defs = jcClassDecl.defs.prepend(makeSetterMethodDecl(jcVariableDecl));
+                    });
+                    super.visitClassDef(jcClassDecl);
+                }
+            });
+        });
+        return false;
+    }
+
+    private JCTree.JCMethodDecl makeSetterMethodDecl(JCTree.JCVariableDecl jcVariableDecl) {
+        ListBuffer<JCTree.JCStatement> statements = new ListBuffer<>();
+        statements.append(treeMaker.Return(treeMaker.Select(treeMaker.Ident(names.fromString("this")), jcVariableDecl.getName())));
+        JCTree.JCBlock body = treeMaker.Block(0, statements.toList());
+        return treeMaker.MethodDef(treeMaker.Modifiers(Flags.PUBLIC), setNewMethodName(jcVariableDecl.getName()), jcVariableDecl.vartype, List.nil(), List.nil(), List.nil(), body, null);
+    }
+
+    private Name setNewMethodName(Name name) {
+        String s = name.toString();
+        return names.fromString("set" + s.substring(0, 1).toUpperCase() + s.substring(1, name.length()));
+    }
+
+}
